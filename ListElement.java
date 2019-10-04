b074b6a2f97bb60d3fd911640f93ac3563b67a89
Jack Zhao <Decafoats108@gmail.com>
2019-10-03 21:22:21 -0600

    commit 7 Deleted loose files. Re-configured deleteElement so that it deletes head node.

--- ListElement.java
+++ ListElement.java
@@ -1,116 +1,121 @@
-import java.util.*; 
+	import java.util.*; 
 
-public class ListElement
-{
-	private ListElement next;
-	private ListElement previous;
-	private int data;
-	
-	public ListElement()
+	public class ListElement
 	{
-		this.data = 0;
-		this.next = null;
-		this.previous = null;
-	}
-
-	/** 
-	* @param data
-	*	an integer to be stored in the node
-	* @return
-	*	None
-	**/
-	public void setData(int data)
-	{
-		this.data = data;
-	}
-	
-	/** 
-	* @param None
-	* @return
-	*	return data stored in the node 
-	**/
-	public int getData()
-	{
-		return this.data;
-	}
-	
-	public void addElement(ListElement le)
-	{
-		ListElement temp = this;
-		ListElement preTemp = null;
+		private ListElement next;
+		private ListElement previous;
+		private int data;
 		
-		if (this.next == null)
+		public ListElement()
 		{
-			this.next = le;
-		
+			this.data = 0;
+			this.next = null;
+			this.previous = null;
+		}
+
+		/** 
+		* @param data
+		*	an integer to be stored in the node
+		* @return
+		*	None
+		**/
+		public void setData(int data)
+		{
+			this.data = data;
 		}
 		
-		else
+		/** 
+		* @param None
+		* @return
+		*	return data stored in the node 
+		**/
+		public int getData()
 		{
-			while (temp.next != null)
+			return this.data;
+		}
+		
+		public void addElement(ListElement le)
+		{
+			ListElement temp = this;
+			ListElement preTemp = null;
+			
+			if (this.next == null)
 			{
-				temp = temp.next;
+				this.next = le;
+			
 			}
 			
-			temp.next = le;
+			else
+			{
+				while (temp.next != null)
+				{
+					temp = temp.next;
+				}
+				
+				temp.next = le;
+			}
 		}
-	}
-	
-	public ListElement getElement(int index)
-	{
-		int counter = 0;
-		ListElement temp = this;
 		
-		while (counter != index)
+		public ListElement getElement(int index)
 		{
-			temp = temp.next;
-			counter++;
+			int counter = 0;
+			ListElement temp = this;
+			
+			while (counter != index)
+			{
+				temp = temp.next;
+				counter++;
+			}
+			
+			return temp;
 		}
 		
-		return temp;
-	}
-	
-	public ListElement deleteElement(int index) 
-	{
-		int counter = 1;
-		ListElement head = this;
-		ListElement delete = this.next;
-		
-		while (counter != index)
+		public ListElement deleteElement(int index) 
 		{
-			head = head.next;
-			delete = delete.next;
-			counter++;
+			int counter = 1;
+			ListElement head = this;
+			ListElement delete = this.next;
+			
+			if (index == 0)
+			{
+				head = this.next;
+				this.next = head;
+			}
+			while (counter != index)
+			{
+				head = head.next;
+				delete = delete.next;
+				counter++;
+			}
+			
+			head.next = delete.next;
+			
+			return head;
 		}
 		
-		head.next = delete.next;
-		
-		return head;
-	}
-	
-	public void printLinkedListHead()
-    { 
-        ListElement temp = this; 
-   
-        System.out.print("LinkedList: "); 
-   
-        while (temp != null) 
+		public void printLinkedListHead()
 		{ 
-            System.out.print(temp.data + " "); 
-            temp = temp.next; 
-        } 
-    } 
-	
-}	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			ListElement temp = this; 
+	   
+			System.out.print("LinkedList: "); 
+	   
+			while (temp != null) 
+			{ 
+				System.out.print(temp.data + " "); 
+				temp = temp.next; 
+			} 
+		} 
+		
+	}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
--- test.java
+++ test.java
@@ -12,27 +12,32 @@ import java.util.*;
 public class test
 {
 	public static void main(String[] args) 
-	{
-		ListElement element1 = new ListElement(); 
-		ListElement element2 = new ListElement(); 
-		ListElement element3 = new ListElement(); 
-		ListElement element4 = new ListElement(); 
-		ListElement element5 = new ListElement(); 
-		
-		element1.setData(5);
-		element2.setData(7);
-		element3.setData(9);
-		element4.setData(11);
-		element5.setData(13);
-		
-		element1.addElement(element2);
-		element1.addElement(element3);
-		element1.addElement(element4);
-		element1.addElement(element5);
-		
-		element1.deleteElement(4);
-	    element1.printLinkedListHead();
-	}
+    {
+          ListElement headElement = new ListElement(); 
+          ListElement element1 = new ListElement(); 
+          ListElement element2 = new ListElement(); 
+          ListElement element3 = new ListElement(); 
+          ListElement element4 = new ListElement(); 
+          ListElement element5 = new ListElement(); 
+
+          headElement.setData(1);
+          element1.setData(5);
+          element2.setData(7);
+          element3.setData(9);
+          element4.setData(11);
+          element5.setData(13);
+
+          headElement.addElement(element1);
+          headElement.addElement(element2);
+          headElement.addElement(element3);
+          headElement.addElement(element4);
+          headElement.addElement(element5);
+         
+          headElement = headElement.deleteElement(1);
+         
+          headElement.printLinkedListHead();
+         
+    }
 	
 
 }
